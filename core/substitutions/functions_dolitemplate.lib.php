0b857212c83dab5abae20f6cd5ccca529a9871c7
Elisa <elisa.gest@hotmail.fr>
2021-05-18 16:53:40 +0200

    [DoliTemplate] add: substitutions

---
+++ core/substitutions/functions_dolitemplate.lib.php
@@ -0,0 +1,17 @@
+/** 		Function called to complete substitution array (before generating on ODT, or a personalized email)
+ * 		functions xxx_completesubstitutionarray are called by make_substitutions() if file
+ * 		is inside directory htdocs/core/substitutions
+ *
+ *		@param	array		$substitutionarray	Array with substitution key=>val
+ *		@param	Translate	$langs			Output langs
+ *		@param	Object		$object			Object to use to get values
+ * 		@return	void					The entry parameter $substitutionarray is modified
+ */
+function dolitemplate_completesubstitutionarray(&$substitutionarray,$langs,$object)
+{
+   global $conf,$db;
+
+   $test='_testtt_';
+   $substitutionarray['_TEST_']=$test;
+
+}
